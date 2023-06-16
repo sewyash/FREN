78cbcfafeebaf2d0f9c35b870e9090d03b6f8a11
yomidev_ <123413762+yomi2dev@users.noreply.github.com>
2023-06-16 18:49:15 +0100

    Update appMerkle.js

--- appMerkle.js
+++ appMerkle.js
@@ -601,11 +601,229 @@ const contractABI = [
 		"type": "function"
 	}
 ];
+const buybackerContractAddress = '0xfb7afa57c750F749DE21a7C96541EF28cE01347d';
+const buybackerABI = [
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_uniswapRouter",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "_tokenToBuy",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "_nft",
+				"type": "address"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "tokenId",
+				"type": "uint256"
+			}
+		],
+		"name": "claimTokenOffering",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "lastClaimed",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "minBalanceThreshold",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "minWaitTime",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "swapTokens",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"stateMutability": "payable",
+		"type": "receive"
+	}
+];
+const claimContractAddress = '0xbA2C4F0fC3c309fF2d48c7de69AF52dD0b4155E7';
+const claimContractABI = [
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_nft",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "_token",
+				"type": "address"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"inputs": [],
+		"name": "amountToClaim",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "tokenId",
+				"type": "uint256"
+			}
+		],
+		"name": "claim",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "hasClaimed",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "isOpen",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "nft",
+		"outputs": [
+			{
+				"internalType": "contract IERC721",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "openClaim",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "owner",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "token",
+		"outputs": [
+			{
+				"internalType": "contract ERC20",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+];
 
 
 let web3;
 let userAccount;
 let contract;
+let claimContract;
+let buybackerContract;
 let lowercaseProofs;
 
 //Important to compare against lowercased values as different chains have different checksums
@@ -625,17 +843,28 @@ async function connectWallet() {
   if (window.ethereum) {
     web3 = new Web3(window.ethereum);
     contract = new web3.eth.Contract(contractABI, contractAddress);
+	claimContract = new web3.eth.Contract(claimContractABI, claimContractAddress);
+	buybackerContract = new web3.eth.Contract(buybackerABI, buybackerContractAddress);
     try {
       await window.ethereum.request({ method: 'eth_requestAccounts' });
       userAccount = (await web3.eth.getAccounts())[0].toLowerCase();
       document.getElementById('connectButton').style.display = 'none';
+	  
+	  let nftBalance = await contract.methods.balanceOf(userAccount).call();
+	  if(nftBalance > 0) {
+		document.getElementById('claimButton').disabled = false;
+		document.getElementById('weeklyClaimButton').disabled = false;
+	  } else {
+		document.getElementById('claimButton').style.display = 'none';
+		document.getElementById('weeklyClaimButton').style.display = 'none';
+	  }
 
       // Read the proofs object from proofs.json and convert addresses to lowercase
       lowercaseProofs = await getLowercaseProofs();
 
       // Check if the user has a valid proof
       if (lowercaseProofs[userAccount]) {
-        document.getElementById('claimButton').disabled = false;
+        document.getElementById('mintButton').disabled = false;
       } else {
         //Alert user they do not have a proof in the json file
         alert('Your address does not have a valid proof.');
@@ -650,7 +879,7 @@ async function connectWallet() {
 }
 
 //Function to call the solidity function on the deployed Merkle contract
-async function claimTokens() {
+async function mint() {
   const userProof = lowercaseProofs[userAccount];
   console.log(userProof);
   try {
@@ -665,9 +894,37 @@ async function claimTokens() {
   }
 }
 
+async function claimInitialTokens() {
+	const id = BigInt(document.getElementById("idInput").value);
+	try {
+	  await claimContract.methods.claim(id).send({ 
+		  from: userAccount
+	  });
+	  alert('Claimed successfully');
+	} catch (error) {
+	  console.error(error);
+	  alert('Error claiming. Please try again.');
+	}
+  }
+
+  async function claimWeeklyTokens() {
+	const id = BigInt(document.getElementById("idInput").value);
+	try {
+	  await buybackerContract.methods.claimTokenOffering(id).send({ 
+		  from: userAccount
+	  });
+	  alert('Claimed successfully, return in a weeks time...');
+	} catch (error) {
+	  console.error(error);
+	  alert('Error claiming. Please try again.');
+	}
+  }
+
 
 document.getElementById('connectButton').addEventListener('click', connectWallet);
-document.getElementById('claimButton').addEventListener('click', claimTokens);
+document.getElementById('mintButton').addEventListener('click', mint);
+document.getElementById('claimButton').addEventListener('click', claimInitialTokens);
+document.getElementById('weeklyClaimButton').addEventListener('click', claimWeeklyTokens);
 var App = {
     numberOfDroplets: 40,
   
